7bd2f99991b791491bd5b50aeb81b447feae8483
ah01 <horcicaa@gmail.com>
2017-10-18 08:32:38 +0200

    Make zip archive from release

--- src/post-build.bat
+++ src/post-build.bat
@@ -1,7 +1,8 @@
 
 set name="%1"
 set src=%~2
-set pub=%~2..\..\..\..\pub
+set pub=%~2..\..\..\..\pub\
+set archiv=%pub%\com-kit_%APPVEYOR_BUILD_NUMBER%.zip
 
 echo Copy from %src%
 echo Copy to %pub%
@@ -10,8 +11,18 @@ if not exist "%pub%" mkdir "%pub%"
 
 :: xcopy "$(TargetDir)*" "$(SolutionDir)..\pub\$(TargetName)\" /D /Y /S
 
- xcopy "%src%*.exe" "%pub%" /D /Y /S
- xcopy "%src%*.dll" "%pub%" /D /Y /S
- xcopy "%src%*.config" "%pub%" /D /Y /S
+ ::xcopy "%src%*.exe" "%pub%" /D /Y /S
+ ::xcopy "%src%*.dll" "%pub%" /D /Y /S
+ ::xcopy "%src%*.config" "%pub%" /D /Y /S
 
- xcopy "%src%*.ids" "%pub%" /D /Y /S
+ ::xcopy "%src%*.ids" "%pub%" /D /Y /S
+
+pushd "%src%"
+
+7z a "%archiv%" "*.exe"
+7z a "%archiv%" "*.dll"
+7z a "%archiv%" "*.config"
+
+7z a "%archiv%" "*.ids" -r
+
+popd
